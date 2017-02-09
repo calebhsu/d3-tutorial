8a12e618bf796f615296c040df235e4d82d8d802
Caleb Hsu <chialo.hsu@colorado.edu>
2017-02-09 11:57:54 -0700

    Creates axis

--- scatterTutorial.js
+++ scatterTutorial.js
@@ -1,7 +1,7 @@
 // First, we will create some constants to define non-data-related parts of the visualization
 var w = 700; // Width of our visualization
 var h = 500; // Height of our visualization
-var xOffset = 40; // Space for x-axis labels
+var xOffset = 110; // Space for x-axis labels
 var yOffset = 100; // Space for y-axis labels
 var margin = 10; // Margin around visualization
 var vals = ['Rank', 'Frequency', 'TFIDF', 'Document Frequency']; // List of data attributes
@@ -13,6 +13,8 @@ d3.csv( 'shakespeare_top100.csv', function( csvData ){
   data = csvData;
 
   // Define scales that convert from the data domain to screen coordinates
+  // This will define scales that convert values
+	// from our data domain into screen coordinates.
   xScale = d3.scale.linear()
              .domain([d3.min( data, function( d ){
                       return parseFloat( d[xVal] );
@@ -29,18 +31,43 @@ d3.csv( 'shakespeare_top100.csv', function( csvData ){
                    })+1])
             .range([h - yOffset - margin, margin]);
 
+	// Next, we will create an SVG element to contain our visualization.
   var svg = d3.select("#pointsSVG").append("svg:svg")
                                    .attr("width", w)
                                    .attr("height", h);
 
-	// This will define scales that convert values
-	// from our data domain into screen coordinates.
-
-
-	// Next, we will create an SVG element to contain our visualization.
-
-
 	// Build axes! (These are kind of annoying, actually...)
+  var xAxis = d3.svg.axis()
+                    .scale(xScale)
+                    .orient("bottom")
+                    .ticks(5);
+
+  var xAxisG = svg.append('g')
+                  .attr('class', 'axis')
+                  .attr('transform', 'translate(0, ' + (h - yOffset) + ')')
+                  .call(xAxis);
+
+  var xLabel = svg.append('text')
+                  .attr('class', 'label')
+                  .attr('x', w/2)
+                  .attr('y', h - margin/2)
+                  .text(xVal);
+
+  var yAxis = d3.svg.axis()
+                    .scale(yScale)
+                    .orient("left")
+                    .ticks(5);
+
+  var yAxisG = svg.append('g')
+                  .attr('class', 'axis')
+                  .attr('transform', 'translate(' + (xOffset) + ')')
+                  .call(yAxis);
+
+  var yLabel = svg.append('text')
+                  .attr('class', 'label')
+                  .attr('x', xOffset/3)
+                  .attr('y', h/2.5)
+                  .text(yVal);
 
 	// Now, we will start actually building our scatterplot!
 		// Select elements
